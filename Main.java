647efba5f289e2686911fe4bd6266a51b715f041
Hiệp <hiepvbhp2005@gmail.com>
2026-08-28 01:57:56 +0000

    Refactor code for JUnit testing

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
 public class Main {
 
+    // Phương thức kiểm tra số chẵn/lẻ
+    public static String checkEvenOdd(int number) {
+        if (number % 2 == 0) {
+            return number + " la so chan";
+        } else {
+            return number + " la so le";
+        }
+    }
+
     public static void main(String[] args) {
 
         // Vòng lặp
         for (int i = 1; i <= 10; i++) {
-
-            // Lệnh rẽ nhánh
-            if (i % 2 == 0) {
-                System.out.println(i + " la so chan");
-            } else {
-                System.out.println(i + " la so le");
-            }
+            System.out.println(checkEvenOdd(i));
         }
     }
 }
