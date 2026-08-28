caa2bde0a1a2e6c74b6e14a1f7639b770fc5b565
Hiệp <hiepvbhp2005@gmail.com>
2026-08-28 01:45:17 +0000

    hiệp

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Vòng lặp
+        for (int i = 1; i <= 10; i++) {
+
+            // Lệnh rẽ nhánh
+            if (i % 2 == 0) {
+                System.out.println(i + " la so chan");
+            } else {
+                System.out.println(i + " la so le");
+            }
+        }
+    }
+}
